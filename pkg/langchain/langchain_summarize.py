fe4e92656c3ebfc6f4d962db650496cb156f0da0
d2jvkpn <d2jvkpn@noreplay.local>
2023-04-25 19:43:25 +0800

    updated langchain_summarize.py

--- pkg/langchain/langchain_summarize.py
+++ pkg/langchain/langchain_summarize.py
@@ -1,8 +1,10 @@
 import os, sys, textract
+from random import sample
 
 from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
+# from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import TokenTextSplitter
 from langchain.docstore.document import Document
 
 fp = sys.argv[1]
@@ -13,7 +15,12 @@ if api_key is None:
 
 llm = OpenAI(temperature=0, openai_api_key=api_key)
 summarize_chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
-text_splitter = CharacterTextSplitter()
+
+text_splitter = TokenTextSplitter(
+  chunk_size = 500,
+  chunk_overlap = 0,
+  length_function = len,
+)
 
 content = textract.process(fp)
 
@@ -21,6 +28,12 @@ docs = []
 for text in text_splitter.split_text(content.decode()):
     docs.append(Document(page_content=text.strip()))
 
+if len(docs) <= 5:
+    sample_docs = docs
+else:
+    xx = sample(docs[1:-1], 3)
+    sample_docs = [docs[0], xx[0], xx[1], xx[2], docs[-1]]
+
 # This model's maximum context length is 4097 tokens, however you requested 8700 tokens (8444 in your prompt; 256 for the completion). Please reduce your prompt; or completion length.
-ans = summarize_chain.run(docs)
+ans = summarize_chain.run(sample_docs)
 print(ans.strip())
