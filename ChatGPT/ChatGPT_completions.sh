0ce68a7f11e26e8a989c670dff435738c928c8f7
d2jvkpn <d2jvkpn@noreplay.local>
2025-01-04 23:28:49 +0800

    ...

--- ChatGPT/ChatGPT_completions.sh
+++ ChatGPT/ChatGPT_completions.sh
@@ -1,44 +1,56 @@
-#! /usr/bin/env bash
-set -eu -o pipefail # -x
-_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+#!/bin/bash
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
+
+#### 1.
 for m in yq jq curl; do
     command -v $m > /dev/null || { >&2 echo "command $m not found"; exit 1; }
 done
 
-app_dir=~/.local/apps/chatgpt
+#### 2.
+app_dir=~/apps/chatgpt
+config=$app_dir/configs/chatgpt.yaml
+
+ls $config > /dev/null
 mkdir -p $app_dir/data
-# ChatGPT_Token=Your_OPENAI_API_Key
-[ -f $app_dir/env ] && source $app_dir/env
 
-[ $# -eq 0 ] && { >&2 echo "Pass your question as argument(s)!"; exit 1; }
-[ -z "${ChatGPT_Token}" ] && { >&2 echo "ChatGPT_Token is unset"; exit 1; }
+# OPENAI_API_Key
+api_key=$(yq .chatgpt.api_key $config)
 
+# gpt-3.5-turbo gpt-4-turbo gpt-4 gpt-4o
+model=$(yq .chatgpt.model $config)
+
+# -x socks5h://localhost:1080
+proxy=$(yq .chatgpt.proxy $config)
+
+if [[ "$api_key$model$proxy" == *"null"* ]]; then
+    >&2 echo "invalid config: $config"
+    exit 1
+fi
+
+#### 3.
 question="$*"
 if [[ "$question" == @* ]]; then
     question=$(cat ${question:1})
 fi
 
-# --arg model "${ChatGPT_Model:-gpt-3.5-turbo}" \
-ChatGPT_Model=${ChatGPT_Model:-gpt-4-turbo}
-
 tag=$(date +%FT%T-%s | sed 's/:/-/g')
-echo "==> $ChatGPT_Model@$tag: $question"
+echo "==> $model@$tag: $question"
 
 ques_file=$app_dir/data/${tag}_quesiton.json
 ans_file=$app_dir/data/${tag}_answer.json
 
 jq -n \
-  --arg model "$ChatGPT_Model" \
+  --arg model "$model" \
   --arg content "$question" \
-  --argjson max_tokens "${ChatGPT_MaxTokens:-1024}" \
-  --argjson temperature "${ChatGPT_Temperature:-1.0}" \
+  --argjson max_tokens "1024" \
+  --argjson temperature "1.0" \
   '{model: $model, messages: [{"role": "user", "content": $content}],
-    max_tokens: $max_tokens, temperature: $temperature}' > $ques_file
+   max_tokens: $max_tokens, temperature: $temperature}' > $ques_file
 
-# CURL_Proxy='-x socks5h://localhost:1081', --silent
-curl https://api.openai.com/v1/chat/completions ${CURL_Proxy:-} \
-  -H 'Content-Type: application/json' -H "Authorization: Bearer $ChatGPT_Token" \
+####
+curl https://api.openai.com/v1/chat/completions $proxy \
+  -H 'Content-Type: application/json' -H "Authorization: Bearer $api_key" \
   -d @$ques_file > $ans_file || { rm $ans_file; exit 1; }
 
 jq -r .choices[].message.content $ans_file || cat $ans_file
---
+++ ChatGPT/ChatGPT_completions.v1.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+for m in yq jq curl; do
+    command -v $m > /dev/null || { >&2 echo "command $m not found"; exit 1; }
+done
+
+app_dir=~/.local/apps/chatgpt
+mkdir -p $app_dir/data
+# ChatGPT_Token=Your_OPENAI_API_Key
+[ -f $app_dir/env ] && source $app_dir/env
+
+[ $# -eq 0 ] && { >&2 echo "Pass your question as argument(s)!"; exit 1; }
+[ -z "${ChatGPT_Token}" ] && { >&2 echo "ChatGPT_Token is unset"; exit 1; }
+
+question="$*"
+if [[ "$question" == @* ]]; then
+    question=$(cat ${question:1})
+fi
+
+# --arg model "${ChatGPT_Model:-gpt-3.5-turbo}" \
+ChatGPT_Model=${ChatGPT_Model:-gpt-4-turbo}
+
+tag=$(date +%FT%T-%s | sed 's/:/-/g')
+echo "==> $ChatGPT_Model@$tag: $question"
+
+ques_file=$app_dir/data/${tag}_quesiton.json
+ans_file=$app_dir/data/${tag}_answer.json
+
+jq -n \
+  --arg model "$ChatGPT_Model" \
+  --arg content "$question" \
+  --argjson max_tokens "${ChatGPT_MaxTokens:-1024}" \
+  --argjson temperature "${ChatGPT_Temperature:-1.0}" \
+  '{model: $model, messages: [{"role": "user", "content": $content}],
+    max_tokens: $max_tokens, temperature: $temperature}' > $ques_file
+
+# CURL_Proxy='-x socks5h://localhost:1081', --silent
+curl https://api.openai.com/v1/chat/completions ${CURL_Proxy:-} \
+  -H 'Content-Type: application/json' -H "Authorization: Bearer $ChatGPT_Token" \
+  -d @$ques_file > $ans_file || { rm $ans_file; exit 1; }
+
+jq -r .choices[].message.content $ans_file || cat $ans_file
+
+{
+    echo -e "\n#### QA"
+    yq -P -oy eval .  $ques_file
+    echo -e "---"
+    yq -P -oy eval .  $ans_file
+} >> $app_dir/data/chatgpt_QA_$(date +%F).yaml
+
+rm $ques_file $ans_file
